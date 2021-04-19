b598393715b9e702ae69f30b36a581a9c703bb1d
Domenic Beall <domenicbeall2@gmail.com>
2021-04-19 12:57:58 +1000

    Added sorting by surname

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,42 @@
+import { useEffect, useState } from "react";
+import TableRow from "./TableRow";
+
 const Table = (props) => {
 
-    const employees = props.employees;
+    const initialEmployees = props.employees;
+
+    const [employees, setEmployees] = useState(props.employees);
+
+    useEffect(() => {
+        setEmployees(props.employees);
+    }, [props.employees]);
+
+    const searchNames = (event) => {
+        setEmployees(initialEmployees.filter((i) => {
+            return i.name.last.toLowerCase().includes(event.target.value.toLowerCase())
+        }));
+    };
 
     return(
-        <ul className="table">
+        <div className="table">
+
+            <div className="sort-form">
+                <input type="text" placeholder="Search by surname..." onKeyUp={searchNames}></input>
+                <label for="sortBy"> Sort by: </label>
+                <select name="sortBy" id="sort">
+                    <option value="First Name">First Name</option>
+                    <option value="Last Name">Last Name</option>
+                    <option value="City">City</option>
+                    <option value="Date of Birth">Date of Birth</option>
+                </select>
+            </div>
+
             {
                 employees.map((employee, i) => {
-                    console.log(employee);
-                    return (<li>{employee.name.first}</li>);
+                    return (<TableRow employee={employee}/>);
                 })
             }
-        </ul>
+        </div>
     );
 
 }
---
+++ src/components/TableRow.js
@@ -0,0 +1,16 @@
+import "./Row.css";
+
+const TableRow = (props) => {
+
+    var employee = props.employee;
+
+    return (
+      <div className="row">
+        <div className="col">{employee.name.first}</div>
+        <div className="col">{employee.name.last}</div>
+      </div>  
+    );
+
+};
+
+export default TableRow;
